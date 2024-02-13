a20b9247de0e6d6aec5a1122a6736f2e498672b6
Marcus Göstasson <m.gostasson@hotmail.com>
2024-02-13 16:25:10 +0100

    Added constructor, getters, setters and one function

---
+++ PigDiceGame/Player.py
@@ -0,0 +1,25 @@
+import dice
+
+
+class player:
+    def __init__(self, name):
+        self.name = name
+        self.total_score = 0
+        self.tossed_amount = 0
+
+    def get_total_score(self):
+        return self.total_score
+
+    def set_total_score(self, value):
+        self.total_score += value
+
+    def get_tossed_amount(self):
+        return self.tossed_amount
+
+    def set_tossed_amount(self):
+        self.tossed_amount += 1
+
+    def throwdice(self, dice):
+        dice_random_number = dice.get_random_number()
+        self.set_tossed_amount()
+        return dice_random_number
