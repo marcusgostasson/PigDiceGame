61a9ddce4710cfea676d98c8228df5071f35e117
Razmus R <Razmus.Rosen@hotmail.com>
2024-02-16 01:55:22 +0100

    Added a little bit on choice 2 in game

--- PigDiceGame/Game.py
+++ PigDiceGame/Game.py
@@ -35,9 +35,23 @@ class Game:
                 player2 = Player.player(player2Name)
                 playing = True
                 while (playing):
-                    playing = self.playerAgainstPlayer(player1)
+                    playing = self.playerPlaying(player1)
                     if (playing is True):
-                        playing = self.playerAgainstPlayer(player2)
+                        playing = self.playerPlaying(player2)
+            elif (choice == 2):
+                playerName = input("What is your name? ")
+                player = Player.player(playerName)
+                choice = int(input("""What difficulty do you want?
+1. Playing against a new born baby
+2. Playing against my uncle that is pretty good with numbers
+3. Playing against Pelle, if you know you know
+4. Completly random no logic"""))
+                computer = Computer.computer(choice)
+                playing = True
+                while (playing):
+                    playing = self.playerPlaying(player)
+                    if (playing is True):
+                        playing = self.computerPlaying(computer)
 
             elif (choice == 3):
                 print("""\nEach turn, a player repeatedly rolls a die until either a 1 is rolled or the player decides to "hold":
@@ -50,7 +64,7 @@ The first player to score 100 or more points wins\n""")
             else:
                 print("Invalid input")
 
-    def playerAgainstPlayer(self, currentPlayer):
+    def playerPlaying(self, currentPlayer):
         dice = Dice.Dice()
         score = currentPlayer.get_total_score()  # Getting score from the player
         gameIsBeingPlayed = True
@@ -82,7 +96,7 @@ The first player to score 100 or more points wins\n""")
                 print("Invalid option!")  # Can make this print in red
         return False
 
-    def playerAgainstComputer(self, computer, dice):
+    def computerPlaying(self, computer):
         dice = Dice.Dice()
         score = computer.get_total_score()  # Getting score from the computer
         gameIsBeingPlayed = True
