ead46eb8ef0cd8ebf1583386696ae451d0c3cb9f
Razmus R <Razmus.Rosen@hotmail.com>
2024-02-23 15:27:18 +0100

    Changed so the naming is consistent text_text

--- PigDiceGame/game.py
+++ PigDiceGame/game.py
@@ -46,13 +46,13 @@ Press 4 if you want to quit""")
                         if (playing is False and player2.get_total_score() < 100):
                             print(player2.get_name() + " surrendered and " + player1.get_name() + " won")
             elif (choice == 2):
-                playerName = input("What is your name? ")
-                player1 = player.Player(playerName)
-                difficulty = int(input("""What difficulty do you want?
+                player_name = input("What is your name? ")
+                player1 = player.Player(player_name)
+                difficulty = self.get_choice_from_user("""What difficulty do you want?
 1. Playing against a new born baby
 2. Playing against my uncle that is pretty good with numbers
 3. Playing against Pelle, if you know you know
-4. Completly random no logic"""))
+4. Completly random no logic""")
                 computer = computer.Computer(difficulty)
                 playing = True
                 while (playing):
@@ -69,38 +69,38 @@ Press 4 if you want to quit""")
             else:
                 print("Invalid input")
 
-    def player_playing(self, currentPlayer):
+    def player_playing(self, current_player):
         die = dice.Dice()
-        score = currentPlayer.get_total_score()  # Getting score from the player
-        gameIsBeingPlayed = True
-        while (gameIsBeingPlayed):
-            print(currentPlayer.get_name() + " you currently have " + str(score) + " point(s)")
-            choice = self.get_choice_from_user(currentPlayer.get_name() + " what do you want to do?:\nPress 1 to toss\nPress 2 to stay\nPress 3 to change name\nPress 4 to surrender\nChoice: ")
+        score = current_player.get_total_score()  # Getting score from the player
+        game_is_being_played = True
+        while (game_is_being_played):
+            print(current_player.get_name() + " you currently have " + str(score) + " point(s)")
+            choice = self.get_choice_from_user(current_player.get_name() + " what do you want to do?:\nPress 1 to toss\nPress 2 to stay\nPress 3 to change name\nPress 4 to surrender\nChoice: ")
 
             if (choice == 1):
-                dieValue = currentPlayer.throw_dice(die)
-                print(currentPlayer.get_name() + " got a " + str(dieValue))
+                dieValue = current_player.throw_dice(die)
+                print(current_player.get_name() + " got a " + str(dieValue))
                 if (dieValue != 1):
                     score += dieValue
-                    gameIsBeingPlayed = self.check_if_winner(score, currentPlayer)
+                    game_is_being_played = self.check_if_winner(score, current_player)
                     continue
                 else:
                     print("Oh you got a " + str(dieValue) + " better luck next time\n")
-                    gameIsBeingPlayed = False
+                    game_is_being_played = False
                     return True
 
             elif (choice == 2):
-                currentPlayer.set_total_score(score)
-                self.players[currentPlayer.get_name()] = currentPlayer.get_total_score()
-                currentPoints = currentPlayer.get_total_score()
-                print(currentPlayer.get_name() + " stayed and now have " + str(currentPoints) + " point(s)\n")
-                gameIsBeingPlayed = False
+                current_player.set_total_score(score)
+                self.players[current_player.get_name()] = current_player.get_total_score()
+                currentPoints = current_player.get_total_score()
+                print(current_player.get_name() + " stayed and now have " + str(currentPoints) + " point(s)\n")
+                game_is_being_played = False
                 return True
 
             elif (choice == 3):
-                old_name = currentPlayer.get_name()
+                old_name = current_player.get_name()
                 value = self.players.pop(old_name)
-                new_name = self.change_name(currentPlayer)
+                new_name = self.change_name(current_player)
                 new_key = new_name
                 self.players[new_key] = value
 
@@ -115,12 +115,12 @@ Press 4 if you want to quit""")
 
     def computer_playing(self, computer):
         die = dice.Dice()
-        difficulty = computer.getDifficulty()
+        difficulty = computer.get_difficulty()
         score = computer.get_total_score()  # Getting score from the computer
-        gameIsBeingPlayed = True
+        game_is_being_played = True
         options = ["toss", "stay"]
         tossCounter = 0 # something with the first toss is 100% toss then i change weight based on how many toss
-        while (gameIsBeingPlayed):
+        while (game_is_being_played):
             print(computer.get_name() + " you currently have " + str(score) + " point(s)")
             pick = random.choices(options, weights=difficulty) # Problem can be with logic to stay at 20 because now the logic is weight based
 
@@ -130,28 +130,28 @@ Press 4 if you want to quit""")
                 print(computer.get_name() + " got a " + str(dieValue))
                 if (dieValue != 1):
                     score += dieValue
-                    gameIsBeingPlayed = self.check_if_winner(score)
+                    game_is_being_played = self.check_if_winner(score)
                     continue
                 else:
                     print("Oh you got a " + str(dieValue) + " better luck next time\n")
-                    gameIsBeingPlayed = False
+                    game_is_being_played = False
                     return True
 
             elif (choice == "stay"):
                 computer.set_total_score(score)
-                currentPoints = computer.get_total_score()
-                print(computer.get_name() + " stayed and now have " + str(currentPoints) + " point(s)\n")
-                gameIsBeingPlayed = False
+                current_points = computer.get_total_score()
+                print(computer.get_name() + " stayed and now have " + str(current_points) + " point(s)\n")
+                game_is_being_played = False
                 return True
 
             else:
                 print("Invalid option!")  # Can make this print in red
         return False
 
-    def check_if_winner(self, score, currentplayer):
+    def check_if_winner(self, score, current_player):
         if (score >= 100):
-            print("You won in " + str(currentplayer.get_tossed_amount()) + " throws!")  # can make this green
-            currentplayer.set_total_score(score)
+            print("You won in " + str(current_player.get_tossed_amount()) + " throws!")  # can make this green
+            current_player.set_total_score(score)
             return False
         else:
             return True
