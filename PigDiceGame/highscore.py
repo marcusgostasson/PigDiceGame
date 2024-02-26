0095c2e212f5108e6da7deed327ed327941460de
Oliver <oliver.brantin0021@stud.hkr.se>
2024-02-26 11:04:21 +0100

    Big changes in highscore

--- PigDiceGame/highscore.py
+++ PigDiceGame/highscore.py
@@ -1,39 +1,19 @@
 class Highscore:
     def __init__(self):
         self.highscores = {}
+    
+    def add_winner(self, player, score):
+        self.highscores[player] = score
         
-        try:
-            open('highscore_list.txt', 'x').close()
-        except FileExistsError:
-            pass
-        
-    def apply_score(self, player_name, score):
-        self.highscores[player_name] = score
-        
-    def get_highscore(self):
-        return self.highscores
-        
-    def seperate_highscores(self, file_path):
-        exsisting_highscores = {}
-        try:
-            with open (file_path, 'r') as file:
-                for line in file:
-                    if line.strip():
-                        name, score = line.split(':')
-                        exsisting_highscores.setdefault(name.strip(), []).append(int(score.strip()))
-        except FileNotFoundError:
-            print("File error")
-        
-        for name, score in self.highscores.items():
-            score = int(score)
-            exsisting_highscores.setdefault(name, [].append(score))
-        
-        sorted_highscores = sorted(exsisting_highscores.items(), key=lambda x: max(x[1]), reverse=True)
-        
-        with open ('highscore_list.txt', 'a') as file:
-            for name, scores in sorted_highscores:
-                for score in scores:
-                    file.write(f"{name} : {score}\n")
-                    
+    def increase_score(self, player):
+        self.highscores[player] += 1
+    
+    def sort_winners_score(self):
+        sorted_highscores = sorted(self.highscores.items(), key=lambda x: x[1], reverse=True)
+        return dict(sorted_highscores)
+    
+    def sort_winners_player(self):
+        sorted_highscore = sorted(self.highscores.items(), key=lambda x: x[0])
+        return dict(sorted_highscore)
         
         
