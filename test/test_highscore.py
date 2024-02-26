0a342e3314339900a8559aac20952787d8a6a417
Oliver <oliver.brantin0021@stud.hkr.se>
2024-02-26 11:42:07 +0100

    Test_highscore cchanges

--- test/test_highscore.py
+++ test/test_highscore.py
@@ -3,24 +3,27 @@ from PigDiceGame.highscore import Highscore
 
 class TestHighscore(unittest.TestCase):
     def setUp(self):
-        self.highscores = Highscore()
-        self.highscores.add_winner("Player1", 2)
-        self.highscores.add_winner("Player2", 4)
+        self.highscore = Highscore()
 
     def test_add_winner(self):
-        self.assertEqual(self.highscores.highscores, {"Player1": 2, "Player2": 4})
+        self.highscore.add_winner("player1")
+        self.assertEqual(self.highscore.highscores["Player1"], 0)
+        
+        self.highscore.add_winner("Player2")
+        self.assertEqual(self.highscore.highscores["Player2"], 0)
+        
+        self.highscore.add_winner("Player1")
+        self.assertEqual(self.highscore.highscores["Player1"], 1)
+        
 
     def test_increase_score(self):
-        self.highscores.increase_score("Player1")
-        self.assertEqual(self.highscores.highscores["Player1"], 3)
+        self.highscore.increase_score("Player1")
+        self.assertEqual(self.highscore.highscores["Player1"], 2)
 
     def test_sort_winners_score(self):
-        sorted_scores = self.highscores.sort_winners_score()
-        self.assertEqual(sorted_scores, {"Player2": 4, "Player1": 2})
-
-    def test_sort_winners_player(self):
-        sorted_players = self.highscores.sort_winners_player()
-        self.assertEqual(sorted_players, {"Player1": 2, "Player2": 4})
+        sorted_scores = self.highscore.sort_winners_score()
+        exp = {"Player2": 0, "Player1": 2}
+        self.assertEqual(sorted_scores, exp)
                 
 if __name__=="__main__":
     unittest.main()
