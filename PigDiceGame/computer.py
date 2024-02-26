5e28f88267dd710080cee47128beb37bb1d23c94
EmilNilsJ <Emil.jansson0082@stud.hkr.se>
2024-02-26 10:32:28 +0100

    Fixed computer

--- PigDiceGame/computer.py
+++ PigDiceGame/computer.py
@@ -1,77 +1,61 @@
 """Computer intelligence"""
 
 import random
+from ast import List
 
 
 class Computer:
     """Class for computer"""
-    def __init__(self):
+    def __init__(self, difficulty):
         """Constructor for computer's sum"""
         self.sum = 0
+        self.difficulty = difficulty
 
-    def hard_difficulty(self):
+    def difficulty_choice(choice):
+        if choice == 1:
+            Computer.easy_difficulty()
+        elif choice == 2:
+            Computer.medium_difficulty()
+        elif choice == 3:
+            Computer.hard_difficulty()
+        elif choice == 4:
+            Computer.random_difficulty()
+
+    def hard_difficulty(self, score):
         """Method for hard_difficulty"""
-        sum = 0
-        while True:
-            random_number = random.randint(1, 6)
-            if random_number != 1:
-                self.sum += random_number
-                sum += random_number
-                print(f"The computer rolled a {random_number}")
-                print(f"Sum of computers numbers: {self.sum}")
-                if sum >= 25:
-                    print(f"The computer decided to hold at: {self.sum}")
-                break
-            elif random_number == 1:
-                print("The computer threw a 1. Sum for the computer is now 0")
-                self.sum = 0
-                break
+        if score >= 25:
+            return random.choices(List, weights=(0, 100))
+        else:
+            return random.choices(List, weights=(100, 0))
 
-    def medium_difficulty(self):
+    def medium_difficulty(self, toss_count):
         """Method for medium_difficulty"""
-        sum = 0
-        for i in range(1, 10):
-            throw_percentage = random.randint(1, i)
-            if throw_percentage == 1:
-                random_number = random.randint(1, 6)
-                if random_number != 1:
-                    self.sum += random_number
-                    sum += random_number
-                    print(f"The computer rolled a {random_number}")
-                    print(f"Sum of computers number: {self.sum}")
-                elif random_number == 1:
-                    print("The computer threw a 1. Sum is now 0")
-                    self.sum = 0
-                    break
-            else:
-                print(f"The computer decided to hold at: {self.sum}")
-                break
+        if toss_count == 0:
+            return random.choices(List, weights=(100, 0))
+        elif toss_count == 1:
+            return random.choices(List, weights=(80, 20))
+        elif toss_count == 2:
+            return random.choices(List, weights=(70, 30))
+        elif toss_count == 3:
+            return random.choices(List, weights=(60, 40))
+        elif toss_count == 4:
+            return random.choices(List, weights=(50, 50))
+        elif toss_count == 5:
+            return random.choices(List, weights=(40, 60))
+        elif toss_count == 6:
+            return random.choices(List, weights=(30, 70))
+        elif toss_count == 7:
+            return random.choices(List, weights=(20, 80))
+        elif toss_count == 8:
+            return random.choices(List, weights=(0, 100))
 
-    def easy_difficulty(self):
+    def easy_difficulty(self, toss_count):
         """Method for easy_difficulty"""
-        random_number = random.randint(1, 6)
-        if random_number != 1:
-            self.sum += random_number
-            print(f"The computer rolled a {random_number}")
-            print(f"Sum of computers number: {self.sum}")
-        elif random_number == 1:
-            print("The computer threw a 1. Sum for the computer is now 0")
-            self.sum = 0
-        print(f"The computer decided to hold at: {self.sum}")
+        if toss_count == 0:
+            return random.choices(List, weights=(100, 0))
+        elif toss_count == 1:
+            return random.choices(List, weights=(0, 100))
 
     def random_difficulty(self):
         """Method for random_difficulty"""
-        while True:
-            random_number = random.randint(1, 6)
-            if random_number != 1:
-                self.sum += random_number
-                print(f"The computer rolled a {random_number}")
-                print(f"Sum of computers number: {self.sum}")
-            elif random_number == 1:
-                print("The computer threw a 1. Sum for the computer is now 0")
-                self.sum = 0
-                break
-            throw_percentage = random.randint(1, 2)
-            if throw_percentage == 1:
-                print(f"The computer decided to hold at: {self.sum}")
-                break
+        return random.choices(List, weights=(50, 50))
