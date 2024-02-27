6000facb049101734e892a805b83b93645acd3a0
Oliver <oliver.brantin0021@stud.hkr.se>
2024-02-27 08:08:39 +0100

    test_highscore

--- test/test_highscore.py
+++ test/test_highscore.py
@@ -7,22 +7,26 @@ class TestHighscore(unittest.TestCase):
 
     def test_add_winner(self):
         self.highscore.add_winner("player1")
-        self.assertEqual(self.highscore.highscores["Player1"], 0)
+        self.assertEqual(self.highscore.highscores["Player1"], 1)
         
         self.highscore.add_winner("Player2")
-        self.assertEqual(self.highscore.highscores["Player2"], 0)
+        self.assertEqual(self.highscore.highscores["Player2"], 1)
         
         self.highscore.add_winner("Player1")
-        self.assertEqual(self.highscore.highscores["Player1"], 1)
+        self.assertEqual(self.highscore.highscores["Player1"], 2)
         
 
     def test_increase_score(self):
+        self.highscore.add_winner("Player1")
         self.highscore.increase_score("Player1")
         self.assertEqual(self.highscore.highscores["Player1"], 2)
 
     def test_sort_winners_score(self):
+        
+        self.highscore.add_winner("Player1")
+        self.highscore.add_winner("Player2")
         sorted_scores = self.highscore.sort_winners_score()
-        exp = {"Player2": 0, "Player1": 2}
+        exp = {"Player2": 1, "Player1": 1}
         self.assertEqual(sorted_scores, exp)
                 
 if __name__=="__main__":
