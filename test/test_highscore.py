df6ac93dafd44f3e7bb11fe656d04250d09f64df
Oliver <oliver.brantin0021@stud.hkr.se>
2024-02-27 08:25:08 +0100

    minor changes

--- test/test_highscore.py
+++ test/test_highscore.py
@@ -1,11 +1,14 @@
+"""Testclass for highscore."""
 import unittest
 from PigDiceGame.highscore import Highscore
 
 class TestHighscore(unittest.TestCase):
+    """Test for highscore class"""
     def setUp(self):
         self.highscore = Highscore()
 
     def test_add_winner(self):
+        """"Test for adding winning"""
         self.highscore.add_winner("player1")
         self.assertEqual(self.highscore.highscores["Player1"], 1)
         
@@ -17,12 +20,13 @@ class TestHighscore(unittest.TestCase):
         
 
     def test_increase_score(self):
+        """Test for increasing score"""
         self.highscore.add_winner("Player1")
         self.highscore.increase_score("Player1")
         self.assertEqual(self.highscore.highscores["Player1"], 2)
 
     def test_sort_winners_score(self):
-        
+        """Test for sorting dictionary"""
         self.highscore.add_winner("Player1")
         self.highscore.add_winner("Player2")
         sorted_scores = self.highscore.sort_winners_score()
