ae9749edee0f21ca0e924edee6a8146b5cead6b0
Oliver <oliver.brantin0021@stud.hkr.se>
2024-03-01 13:16:13 +0100

    small fixes

--- PigDiceGame/highscore.py
+++ PigDiceGame/highscore.py
@@ -1,15 +1,23 @@
+"""Highscore class."""
+
+
 class Highscore:
+    """Class to initiate highscore."""
+
     def __init__(self):
+        """Initialize highsscore list."""
         self.highscores = {}
-    
+
     def add_winner(self, player):
+        """Add winner a winner."""
         if player in self.highscores:
             self.highscores[player] += 1
         else:
             score = 1
             self.highscores[player] = score
-        
+
     def get_name_and_highscore(self):
+        """Class method to get name and score."""
         names = []
         values = []
 
@@ -21,6 +29,7 @@ class Highscore:
         return names, values
 
     def sorted_list(self):
-        sorted_highscores = sorted(self.highscores.items(), key=lambda x: x[1], reverse=True)
+        """Class method to sort highscores."""
+        sorted_highscores = sorted(self.highscores.items(), key=lambda x: x[1],
+                                   reverse=True)
         return sorted_highscores
-
