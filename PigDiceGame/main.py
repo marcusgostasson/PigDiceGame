c2c1dd82e8ec2fd0271abb7617fb60a8f6049f1f
Marcus Göstasson <m.gostasson@hotmail.com>
2024-03-02 20:45:17 +0100

    Removed few lines in main (have it in word)

--- PigDiceGame/main.py
+++ PigDiceGame/main.py
@@ -1,31 +1,18 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-"""
-Lets play a game of "Guess my number".
-
-I will be thinking af a number between 1 and 100.
-You shall try to guess the number.
-
-I will let you know if your guess is lower or higher than
-the number that I am thinking of.
-
-You have 5 guesses. Then you loose.
-
-"""
-
-#import shell
+"""Main Class."""
+# import shell
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 from PigDiceGame import game
+
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 
 def main():
     """Execute the main program."""
-    #print(__doc__)
-    #shell.Shell().cmdloop()
+    # print(__doc__)
+    # shell.Shell().cmdloop()
     game.Game().start_game()
 
 
