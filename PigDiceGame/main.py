c9e8939ca20c64abe0d799825dc62f00e4b6e958
Marcus Göstasson <m.gostasson@hotmail.com>
2024-03-05 15:48:15 +0100

    Removed string in Main

--- PigDiceGame/main.py
+++ PigDiceGame/main.py
@@ -5,13 +5,6 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from PigDiceGame import game, highscore
 
-"""
-"Pig" is a dice game for two players aiming to reach 100 points first.
-Roll dice, avoiding ones to accumulate points.
-Decide whether to roll again or pass to the next player.
-Balancing risk and reward is key 🎲.
-"""
-
 
 def main():
     """Execute the main program."""
